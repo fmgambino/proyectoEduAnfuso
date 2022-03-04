8bf3e5de813bac73ec9004c923f4cd1699562fea
fmgambino <fernando.m.gambino@gmail.com>
2022-03-03 21:50:37 -0300

    Se Actualiza Esquema y PCB LayOut

---
+++ software/TEST-BUTON-WI-FI-PULLUP/TEST-BUTON-WI-FI-PULLUP.ino
@@ -0,0 +1,22 @@
+int D8 = 15;
+int D4 = 2;
+int val;
+
+void setup() {
+ 
+  pinMode(D8, INPUT_PULLUP);
+  pinMode(D4, OUTPUT);
+
+}
+
+void loop() {
+
+  val=digitalRead(D8);
+
+  if  (val==HIGH){
+    digitalWrite(D4,HIGH);
+    }
+  else { digitalWrite(D4,LOW);
+  }
+  
+}
